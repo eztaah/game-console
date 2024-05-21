d11c3ee0956dab39231ac36321bf6c16cd9587ff
eztaah <eztaah@gmail.com>
2024-05-21 17:40:56 +0200

    code pour activer les leds sur le jeu du simon en C

---
+++ jeu-simon-leds.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=jeu-simon-c.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/jeu-simon-c.production.hex
