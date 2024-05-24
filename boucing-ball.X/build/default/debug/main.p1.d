4fc745219b76e6a6bcf4bd89530995a18231c628
eztaah <eztaah@gmail.com>
2024-05-24 12:45:09 +0200

    code pour boucing ball

---
+++ boucing-ball.X/build/default/debug/main.p1.d
@@ -0,0 +1,8 @@
+build/default/debug/main.p1:  \
+main.c  \
+bit_settings.h  \
+spi_pic18f.h  \
+ili9341.h  \
+font.h  \
+bitmap.h  \
+time_delay.h 
---
+++ boucing-ball.X/build/default/production/time_delay.p1.d
@@ -0,0 +1,3 @@
+build/default/production/time_delay.p1:  \
+time_delay.c  \
+time_delay.h 
