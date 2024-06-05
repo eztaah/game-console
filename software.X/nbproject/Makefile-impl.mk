62a25661a7b18734258400e6d42a0954e975537e
eztaah <eztaah@gmail.com>
2024-06-05 20:59:56 +0100

    rename

--- software.X/nbproject/Makefile-impl.mk
+++ software.X/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=game_console_software.X
+PROJECTNAME=software.X
 
 # Active Configuration
 DEFAULTCONF=default
